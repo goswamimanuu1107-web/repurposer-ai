2150126e784b2629e5b2694e418834a7ed137c6c
Manish <289146115+goswamimanuu1107-web@users.noreply.github.com>
2026-06-21 21:46:27 +0530

    add landing page, redesign dashboard, URL support

--- server.js
+++ server.js
@@ -125,5 +125,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Repurposer running on http://localhost:${PORT}`));
